aeba791df6db680f5c3bc8cd490d2402a5467134
bruce-sha <yingx_xu@163.com>
2018-07-29 23:42:32 +0800

    首页添加彩带动画效果(未解决移动端初次加载无法显示问题)

--- README.md
+++ README.md
@@ -19,3 +19,10 @@ npm run build --report
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+## about dpr
+
+设备像素比简称dpr,其定义了物理像素和设备独立像素的对应关系
+设备像素比 = 物理像素 / 设备独立像素 在js中可以通过window.devicePixelRatio获取到当前设备的dpr
+在css中,可以通过-webkit-device-pixel-ratio , -webkit-min-device-pixel-ratio和-webkit-max-device-pixel-ratio
+进行媒体查询,对不同dpr的设备,做一些样式适配。
--- src/page/home.vue
+++ src/page/home.vue
@@ -1,9 +1,10 @@
 <template>
-    <div>
+    <div class="home" @click="init" @touchmove="init">
         <h3>home</h3>
         <router-link tag="a" to="about">about</router-link>
         <router-link tag="a" to="blog">blog</router-link>
         <router-link tag="a" to="category">category</router-link>
+        <canvas ref="canvas"></canvas>
         <!--router-link的几种写法>
         1.<router-link to="home"></router-link>
         2.使用v-bind的js表达式
@@ -24,12 +25,74 @@
 </template>
 <script>
 export default {
-}
+  data() {
+    return {
+      RIBBON_WIDE: 90,
+      r: 0,
+      PI_2: Math.PI * 2,
+      cos: Math.cos,
+      random: Math.random
+    };
+  },
+  mounted() {
+    this.canvasRibbon = this.$refs.canvas;
+    this.ctx = this.canvasRibbon.getContext("2d");
+    this.dpr = window.devicePixelRatio || 1;
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.canvasRibbon.width = this.width * this.dpr;
+    this.canvasRibbon.height = this.height * this.dpr;
+    this.ctx.scale(this.dpr, this.dpr);
+    this.ctx.globalAlpha = 0.6;
+    this.init();
+  },
+
+  methods: {
+    init() {
+      this.ctx.clearRect(0, 0, this.width, this.height);
+      this.path = [
+        { x: 0, y: this.height * 0.7 + this.RIBBON_WIDE },
+        { x: 0, y: this.height * 0.7 - this.RIBBON_WIDE }
+      ];
+      while (this.path[1].x < this.width + this.RIBBON_WIDE) {
+        this.draw(this.ctx, this.path[0], this.path[1]);
+      }
+    },
+    draw(ctx, start, end) {
+      ctx.beginPath();
+      ctx.moveTo(start.x, start.y);
+      ctx.lineTo(end.x, end.y);
+      let nextX = end.x + (Math.random() * 2 - 0.25) * this.RIBBON_WIDE,
+        nextY = this.geneY(end.y);
+      ctx.lineTo(nextX, nextY);
+      ctx.closePath();
+      this.r -= this.PI_2 / -50;
+      ctx.fillStyle =
+        "#" +
+        (
+          ((this.cos(this.r) * 127 + 128) << 16) |
+          ((this.cos(this.r + this.PI_2 / 3) * 127 + 128) << 8) |
+          (this.cos(this.r + this.PI_2 / 3 * 2) * 127 + 128)
+        ).toString(16);
+      ctx.fill(); // 根据当前样式填充路径
+      this.path[0] = this.path[1]; // 起点更新为当前终点
+      this.path[1] = { x: nextX, y: nextY }; // 更新终点
+    },
+    // 获取下一路径终点的y坐标值
+    geneY(y) {
+        var temp = y + (Math.random() * 2 - 1.1) * this.RIBBON_WIDE;
+        return (temp > this.height || temp < 0) ? this.geneY(y) : temp
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
-    a {
-        display: inline-block;
-        height: 100px;
-        width: 100px;
-    }
+.home {
+  position: relative;
+  a {
+    display: inline-block;
+    height: 100px;
+    width: 100px;
+  }
+}
 </style>
